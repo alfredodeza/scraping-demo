e7057d6ccd24ff27996dcf1183ab8603c00bab32
Alfredo Deza <alfredo@deza.pe>
2021-10-17 10:00:57 -0400

    uncomment creation sql script

--- sql_files/create.sql
+++ sql_files/create.sql
@@ -1,8 +1,8 @@
 -- Uncomment when db doesn't exist
-CREATE DATABASE exploitdb
-  ENCODING 'UTF8';
-
-CREATE TABLE IF NOT EXISTS exploits (
-	id integer primary key,
-	exploit_id VARCHAR(60),
-	cves TEXT);
+-- CREATE DATABASE exploitdb
+--   ENCODING 'UTF8';
+--
+-- CREATE TABLE IF NOT EXISTS exploits (
+-- 	id integer primary key,
+-- 	exploit_id VARCHAR(60),
+-- 	cves TEXT);
